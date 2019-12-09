deef138391d879d77b5ebe8a95aadf26a072bdb1
luydjmix2 <luydjmix@hotmail.com>
2019-12-09 15:48:20 -0500

    ingreso de shell

--- modo_grafico_centos_7.sh
+++ modo_grafico_centos_7.sh
@@ -1,1 +1,17 @@
-
+yum groupinstall "GNOME Desktop" -y
+read var1
+echo "Yes = Y"
+if [ "$var1" == "Y" ]; then
+yum groupinstall "Server with GUI"
+fi
+echo "Yes = Y"
+read var2
+if [ "$var2" == "Y" ]; then
+unlink /etc/systemd/system/default.target
+fi
+echo "Yes = Y"
+read var3
+if [ "$var3" == "Y" ]; then
+ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+fi
+echo "ejecute el comando 'reboot'"
