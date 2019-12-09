fc83a8e20dcb96c8b2321005102d49878fa8d9a7
luydjmix2 <luydjmix@hotmail.com>
2019-12-09 16:34:53 -0500

    Update modo_grafico_centos_7.sh

--- modo_grafico_centos_7.sh
+++ modo_grafico_centos_7.sh
@@ -6,11 +6,11 @@ function preflight_check() {
   fi
 }
 yum groupinstall "GNOME Desktop" -y
-preflight_check "ejecute sentencia" $? "se necesita ejecutar la sentencia para continuar"
+
 yum groupinstall "Server with GUI"
-preflight_check "ejecute sentencia" $? "se necesita ejecutar la sentencia para continuar"
+
 unlink /etc/systemd/system/default.target
-preflight_check "ejecute sentencia" $? "se necesita ejecutar la sentencia para continuar"
+
 ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-preflight_check "ejecute sentencia" $? "se necesita ejecutar la sentencia para continuar"
+
 echo "ejecute el comando 'reboot'"
